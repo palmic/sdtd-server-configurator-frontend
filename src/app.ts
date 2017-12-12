1e90fcd6ba8a2242930597bf166606ee6145b676
Michal Palma <michal.palma@palmam.local>
2017-12-12 20:52:13 +0100

    xmldom

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+import {DOMParser, XMLSerializer} from 'xmldom';
+
 export class App {
-  message = 'Hello World!';
+
+	public error = "";
+
+	public content = "";
+
+	public doc :Document;
+
+	fileLoaded = (file, data) => {
+		this.error = "";
+		let reader = new FileReader();
+		reader.readAsText(file);
+		reader.onloadend = () => {
+			this.doc = new DOMParser().parseFromString(reader.result);
+			this.content = (new XMLSerializer()).serializeToString(this.doc);
+		};
+	};
+
+	loadingInProgress = (file, data) => {
+
+	};
+
+	loadError = (file, data) => {
+		this.content = "";
+		this.doc = null;
+		this.error = data;
+	}
+
 }
