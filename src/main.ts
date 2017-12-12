6f0af0d893411cbcb364fea52aee12c628766c86
Michal Palma <michal.palma@palmam.local>
2017-12-12 23:21:42 +0100

    trying to add materialize

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,20 @@ import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature('resources');
+	aurelia.use
+		.standardConfiguration()
+		.feature('resources');
 
-  aurelia.use.plugin("aurelia-files");
+	aurelia.use.plugin("aurelia-files");
+	aurelia.use.plugin('aurelia-materialize-bridge', b => b.useAll());
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+	if (environment.debug) {
+		aurelia.use.developmentLogging();
+	}
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
+	if (environment.testing) {
+		aurelia.use.plugin('aurelia-testing');
+	}
 
-  aurelia.start().then(() => aurelia.setRoot());
+	aurelia.start().then(() => aurelia.setRoot());
 }
